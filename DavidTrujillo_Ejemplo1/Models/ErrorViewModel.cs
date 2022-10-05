37420c32c5cfbbe35320a165bfb29f7a4bca823e
Truji2002 <datrujillo2002@gmail.com>
2022-10-05 09:41:54 -0500

    Agregar archivos de proyecto.

---
+++ DavidTrujillo_Ejemplo1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DavidTrujillo_Ejemplo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
